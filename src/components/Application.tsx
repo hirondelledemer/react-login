cbe9fce6791b4d7ce8245f54917ec6e08f41f202
Meda <mac@macs-MacBook-Pro.local>
2024-05-07 15:46:46 +0300

    initial setup

---
+++ src/components/Application.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Application: React.FC = () => {
+  return <div>Welcome</div>;
+};
+
+export default Application;
---
+++ tools/webpack/webpack.rules.js
@@ -0,0 +1,41 @@
+const { inDev } = require('./webpack.helpers');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = [
+  {
+    // typescript
+    test: /\.tsx?$/,
+    exclude: /(node_modules|\.webpack)/,
+    use: {
+      loader: 'ts-loader',
+      options: {
+        transpileOnly: true,
+      },
+    },
+  },
+  {
+    // css
+    test: /\.css$/,
+    use: [
+      { loader: inDev() ? 'style-loader' : MiniCssExtractPlugin.loader },
+      { loader: 'css-loader' },
+    ],
+  },
+  {
+    // scss
+    test: /\.s[ac]ss$/i,
+    use: [
+      { loader: inDev() ? 'style-loader' : MiniCssExtractPlugin.loader },
+      { loader: 'css-loader' },
+      { loader: 'sass-loader' },
+    ],
+  },
+  {
+    // assets
+    test: /\.(gif|jpe?g|tiff|png|webp|bmp|svg|eot|ttf|woff|woff2)$/i,
+    type: 'asset',
+    generator: {
+      filename: 'assets/[hash][ext][query]',
+    },
+  },
+];
