7df6c91b1ccff22f0cda9d417f3b9c6645b1a16a
Meda <mac@macs-MacBook-Pro.local>
2024-05-13 23:04:05 +0300

    remove access to login paga for logged in user

--- src/app/Application.tsx
+++ src/app/Application.tsx
@@ -7,12 +7,15 @@ import Servers from '@src/pages/Servers';
 import { HOME, LOGIN, SERVERS } from '@src/utils/consts/routes';
 import '../index.css';
 import NotFound from '@src/pages/NotFound';
+import { PublicRoutes } from './PublicRoutes';
 
 const Application: React.FC = () => (
   <BrowserRouter>
     <Routes>
       <Route path={HOME} element={<Home />} />
-      <Route path={LOGIN} element={<Login />} />
+      <Route element={<PublicRoutes />}>
+        <Route path={LOGIN} element={<Login />} />
+      </Route>
       <Route element={<ProtectedRoutes />}>
         <Route path={SERVERS} element={<Servers />} />
       </Route>
